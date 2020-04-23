472e9ed08dd3bbfd2c6d0a2f947b158d6ddbdd11
Mark Kop <mk.marcelo.mk@gmail.com>
2020-04-23 00:36:40 -0300

    doc: script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,8 @@
+/**
+ * Insert time delimiter ":" automatically when
+ * inputing time stamp in the format "HH:MM:SS"
+ * @param { Object } event
+ */
 function formatTimeInput(event) {
   const input = event.target
   let characters = input.value.split('')
@@ -15,11 +20,18 @@ function formatTimeInput(event) {
   input.value = characters.join('')
 }
 
+/**
+ * Add a listener that formats time inputs on all
+ * time inputs
+ */
 function listenAndFormatTimeInput() {
   const inputs = Array.from(document.querySelectorAll('.time-input'))
   inputs.forEach((input) => input.addEventListener('input', formatTimeInput))
 }
 
+/**
+ * Add listeners on page load
+ */
 window.addEventListener('load', () => {
   listenAndFormatTimeInput()
 })
